f04b40bdc9c906f9bd8ea9acd4fe2b7da9b35350
Omkar-Thrive360 <omkarchougule.thrive360@gmail.com>
2024-09-28 04:02:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Simple JavaScript animations for smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
